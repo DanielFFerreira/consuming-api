3ba6df9ff9942efc8ba61dcebb6d8ec85ec83461
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-02-04 18:58:27 -0300

    (server) enabling cors and using access

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const cors = require('cors')
 const express = require('express');
 const app = express();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   return res.json([
     {name: 'Daniel Ferreira'},
-    {idade: 25}
+    {idade: 25},
+    {cidade: 'Bebedouro'},
+    {estado: 'SP'}
   ]);
 });
 
