6c324b5afb71b79746c8ac8a997301855ed3d249
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2021-02-04 22:55:37 -0300

    accessing backend axios API

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const cors = require('cors')
 const express = require('express');
 const app = express();
+const axios = require('axios')
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  return res.json([
-    {name: 'Daniel Ferreira'},
-    {cidade: 'Bebedouro'},
-    {estado: 'SP'}
-  ]);
+app.get('/', async(req, res) => {
+
+  try {
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+
+    return res.json(data)
+    
+  } catch (error) {
+    console.error(error)
+  }
 });
 
 app.listen('4567')
